2e54daf9202fd59d53d537d7217f353c4d05abe9
REZA <reza.ngr8@gmail.com>
2023-01-01 09:29:01 -0800

    add navbar & foteer & portofspeech in components

---
+++ components/Foteer.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Foteer() {
+  return <div>Foteer</div>;
+}
---
+++ components/PartOfSpeech.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PartOfSpeech() {
+  return <div>PartOfSpeech</div>;
+}
