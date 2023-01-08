1fe118bc0cbfab901f6d5a56eb4e268a85f16906
REZA <reza.ngr8@gmail.com>
2023-01-08 05:49:55 -0800

    add icon site

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,12 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "./api/Store";
-import { Varela } from '@next/font/google'
 
 function App({ Component, pageProps }: AppProps) {
-  // const varela = Varela({
-  //   weight: '400',
-  // })
+
   return (
     <Provider store={store}>
-      {/* <main className={varela.className}> */}
        <Component {...pageProps} /> 
-      {/* </main> */}
     </Provider>
   );
 }
