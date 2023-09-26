804b7f4f05a6c635990a86d27ec5b6428d93afe3
Reza <reza.ngr8@gmail.com>
2023-09-26 10:29:13 -0700

    update ui examples

--- components/Example.tsx
+++ components/Example.tsx
@@ -14,44 +14,47 @@ export default function Example({ examples }: any) {
     }
   };
   return (
-    <div className="border-2 border-l-8 mx-3 p-5 rounded-lg border-[#264653] mb-4">
+    <div className='border-2 border-l-8 mx-3 p-5 rounded-lg border-[#264653] mb-4 bg-white'>
       <div className="text-2xl font-semibold mb-2">
         Examples
       </div>
-      <ul>
-        {status === "success" ? (
-          Examples?.slice(0, count).map((example, index) => (
-            <div
-              key={index}
-              className="flex text-gray-600 font-medium text-lg mb-3"
+        <ul className={count > 10 ? 'max-h-[72vh] overflow-auto': 'max-h-[72vh] overflow-auto'}>
+            {status === "success" ? (
+                Examples?.slice(0, count).map((example, index) => (
+                    <div
+                        key={index}
+                        className="flex text-gray-600 font-medium text-lg mb-3"
+                    >
+                        {example !== undefined ? (
+                            <>
+                                <span className="mr-2">{index + 1}.</span>
+                                <li>{example !== undefined && example}</li>
+                            </>
+                        ) : (
+                            <span>Empty</span>
+                        )}
+                    </div>
+                ))
+            ) : (
+                <div>
+                    <Skeleton
+                        height={30}
+                        count={6}
+                        className="mb-2 w-[250px] sm:w-[280px]"
+                    />
+                    <Skeleton width={150} height={30} />
+                </div>
+            )}
+        </ul>
+        {Examples.length > count && (
+            <button
+                className="flex mx-auto text-gray-500 font-medium text-lg mt-3"
+                onClick={handelClick}
             >
-              {example !== undefined ? (
-                <>
-                  <span className="mr-2">{index + 1}.</span>
-                  <li>{example !== undefined && example}</li>
-                </>
-              ) : (
-                <span>Empty</span>
-              )}
-            </div>
-          ))
-        ) : (
-          <div>
-            <Skeleton
-              height={30}
-              count={6}
-              className="mb-2 w-[250px] sm:w-[280px]"
-            />
-            <Skeleton width={150} height={30} />
-          </div>
+                More
+            </button>
         )}
-      </ul>
-      <button
-        className="flex mx-auto text-gray-500 font-medium text-lg mt-3"
-        onClick={handelClick}
-      >
-        More
-      </button>
+
     </div>
   );
 }
